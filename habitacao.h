1efbb0f3fc23a69472c46ce070e3847a4c29ff2d
Astr0Zer0 <guilhermeproencaaraujo@gmail.com>
2023-12-28 09:19:14 +0000

    class habitacao started

---
+++ habitacao.h
@@ -0,0 +1,14 @@
+//
+// Created by guilh on 28/12/2023.
+//
+
+#ifndef HOUSE_SIMULATOR_HABITACAO_H
+#define HOUSE_SIMULATOR_HABITACAO_H
+
+
+class habitacao {
+
+};
+
+
+#endif //HOUSE_SIMULATOR_HABITACAO_H
