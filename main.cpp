19f4f96256192191302794dab0b3060b882f2dd5
Astr0Zer0 <guilhermeproencaaraujo@gmail.com>
2023-12-31 14:20:55 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,20 @@
 int main() {
     house fireHouse;
     fireHouse.newRoom(1, 1);
-    fireHouse.newComponent("r0", 'p', "liga");
+    fireHouse.newComponent("r0", 'p', "on");
+    fireHouse.newComponent("r0", 'd', "heater");
+    fireHouse.associateDeviceToProcessor("r0", "p0", "d0");
     fireHouse.newComponent("r0", 's', "temperature");
-    fireHouse.addRule("r0", "p0", "s0", "equal_to", 90);
-    fireHouse.addRule("r0", "p0", "s0", "less_than", 90);
-    fireHouse.addRule("r0", "p0", "s0", "greater_than", 90);
-    fireHouse.addRule("r0", "p0", "s0", "in_between", 90, 100);
-    fireHouse.saveProcessor("r0", "p0", "ola");
-    fireHouse.removeComponent("r0", 'p', "p0");
-    fireHouse.restoreProcessor("ola");
-    std::cout << fireHouse.describeRoom("r0") << std::endl;
+    fireHouse.addRule("r0", "p0", "s0", "less_than", 5);
+    fireHouse.newComponent("r0", 'p', "off");
+    fireHouse.associateDeviceToProcessor("r0", "p1", "d0");
+    fireHouse.addRule("r0", "p1", "s0", "greater_than", 5);
+    std::cout << fireHouse.showPropertysOfRoom("r0") << std::endl;
+    for (int i = 0; i < 20; ++i) {
+        fireHouse.carryOut();
+        std::cout << fireHouse.showPropertysOfRoom("r0") << std::endl;
+    }
+
+
     return 0;
 }
